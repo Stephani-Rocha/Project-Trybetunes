de4b2538f5f8eb19f1267748cf7679f355d42214
Stephani Rocha <gomes.stephani19@gmail.com>
2022-05-02 17:11:29 -0300

    Implementa requisito 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      userName: '',
+    };
+  }
+
+  componentDidMount() {
+    this.chamaGetUser();
+  }
+
+  chamaGetUser = async () => {
+    this.setState({ loading: true });
+    const funcGetUser = await getUser();
+    this.setState({ userName: funcGetUser.name, loading: false });
+  }
+
   render() {
+    const { loading, userName } = this.state;
     return (
-      <header data-testid="header-component" />
+      <header data-testid="header-component">
+        { loading && <Carregando /> }
+        <p data-testid="header-user-name">
+          { userName }
+        </p>
+
+      </header>
     );
   }
 }
