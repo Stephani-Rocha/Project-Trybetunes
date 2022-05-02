3e9005dc9a84153e1d995bff1d71148e8d4b11f9
Stephani Rocha <gomes.stephani19@gmail.com>
2022-05-02 17:55:18 -0300

    Implementa requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nomeArtista: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      nomeArtista: target.value,
+    });
+  }
+
   render() {
+    const { nomeArtista } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          value={ nomeArtista }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          disabled={ nomeArtista.length <= 1 }
+          data-testid="search-artist-button"
+        >
+          Pesquisa
+        </button>
       </div>
     );
   }
