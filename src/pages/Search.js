662d877830bb507266b7b6be8595441a61ec1a93
Stephani Rocha <gomes.stephani19@gmail.com>
2022-05-03 21:22:59 -0300

    Implementa requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Carregando from '../components/Carregando';
 
 class Search extends Component {
   constructor() {
     super();
     this.state = {
       nomeArtista: '',
+      loading: false,
+      listaDeAlbuns: [],
+      semRetorno: false,
+      resposta: false,
+      guardaArtista: '',
     };
   }
 
@@ -15,24 +23,77 @@ class Search extends Component {
     });
   }
 
-  render() {
+  buttonPesquisa = () => {
+    this.setState({
+      nomeArtista: '',
+    });
+  }
+
+  semRetorno = () => {
+    const { listaDeAlbuns } = this.state;
+    if (listaDeAlbuns.length === 0) {
+      this.setState({ semRetorno: true });
+    }
+  }
+
+  pesquisaArtista = async () => {
+    this.setState({ loading: true });
     const { nomeArtista } = this.state;
+    await searchAlbumsAPI(nomeArtista);
+    this.setState({ listaDeAlbuns: await searchAlbumsAPI(nomeArtista), loading: false });
+    this.setState({ resposta: true, guardaArtista: nomeArtista });
+    this.semRetorno();
+    this.buttonPesquisa();
+  }
+
+  render() {
+    const { nomeArtista,
+      loading,
+      listaDeAlbuns,
+      semRetorno,
+      resposta,
+      guardaArtista } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <input
-          type="text"
-          data-testid="search-artist-input"
-          value={ nomeArtista }
-          onChange={ this.handleChange }
-        />
-        <button
-          type="button"
-          disabled={ nomeArtista.length <= 1 }
-          data-testid="search-artist-button"
-        >
-          Pesquisa
-        </button>
+        { loading ? (
+          <Carregando />
+        ) : (
+          <div>
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              value={ nomeArtista }
+              onChange={ this.handleChange }
+            />
+            <button
+              type="button"
+              disabled={ nomeArtista.length <= 1 }
+              data-testid="search-artist-button"
+              onClick={ this.pesquisaArtista }
+            >
+              Pesquisa
+            </button>
+          </div>
+        )}
+        { resposta
+        && (<p>{`Resultado de álbuns de: ${guardaArtista}`}</p>)}
+        { listaDeAlbuns.length > 0 && (
+          listaDeAlbuns.map((albuns) => (
+            <div key={ albuns.collectionId }>
+              <img src={ albuns.artworkUrl100 } alt="imagem do album" />
+              <Link
+                data-testid={ `link-to-album-${albuns.collectionId}` }
+                to={ `/album/${albuns.collectionId}` }
+              >
+                { albuns.collectionName }
+              </Link>
+              <p>
+                { albuns.artistName }
+              </p>
+            </div>
+          )))}
+        { semRetorno && <p>Nenhum álbum foi encontrado</p> }
       </div>
     );
   }
