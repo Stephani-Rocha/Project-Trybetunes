7264c30a812722aea94db93f435a57053cac43c0
Stephani Rocha <gomes.stephani19@gmail.com>
2022-05-13 10:15:18 -0300

    Implementando requisito 12 e finalizado requisito 13

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import Carregando from '../components/Carregando';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
+  constructor() {
+    super();
+    this.state = {
+      carregando: false,
+      listaFavoritas: [],
+      checked: true,
+    };
+  }
+
+  componentDidMount() {
+    this.musicasFavoritas();
+  }
+
+  musicasFavoritas = async () => {
+    this.setState({ carregando: true });
+    const chamaFunc = await getFavoriteSongs();
+    this.setState({ listaFavoritas: chamaFunc, carregando: false });
+  }
+
   render() {
+    const { listaFavoritas, carregando, checked } = this.state;
+
     return (
       <div data-testid="page-favorites">
         <Header />
+        { carregando ? (<Carregando />)
+          : (
+            <div>
+              { listaFavoritas.map((item) => (
+                <div key={ item.trackId }>
+                  <MusicCard
+                    trackName={ item.trackName }
+                    previewUrl={ item.previewUrl }
+                    trackId={ item.trackId }
+                    favoritas={ this.musicasFavoritas }
+                    path="favorites"
+                    checked={ checked }
+                  />
+                </div>
+              ))}
+            </div>)}
       </div>
     );
   }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Carregando from '../components/Carregando';
 
 class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      carregando: false,
+      nome: '',
+      email: '',
+      descrição: '',
+      imagem: '',
+    };
+  }
+
+  componentDidMount() {
+    this.exibiPerfil();
+  }
+
+  exibiPerfil = async () => {
+    this.setState({ carregando: true });
+    const chamaFunc = await getUser();
+    console.log(chamaFunc);
+    this.setState({
+      carregando: false,
+      nome: chamaFunc.name,
+      email: chamaFunc.email,
+      descrição: chamaFunc.description,
+      imagem: chamaFunc.image });
+  }
+
   render() {
+    const { carregando, nome, email, descrição, imagem } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        { carregando ? (<Carregando />)
+          : (
+            <div>
+              <h3>Nome</h3>
+              <p>{nome}</p>
+              <h3>Email</h3>
+              <p>{email}</p>
+              <h3>Descrição</h3>
+              <p>{descrição}</p>
+              <h3>Imagem</h3>
+              <img src={ imagem } alt={ imagem } data-testid="profile-image" />
+              <Link to="/profile/edit"> Editar perfil </Link>
+            </div>
+          )}
       </div>
     );
   }
