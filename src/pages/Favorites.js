dcc580f3425f103f0ee542422e7ac6cd8a10d23d
Stephani Rocha <gomes.stephani19@gmail.com>
2022-05-13 12:08:41 -0300

    Corrige problemas com a page Favorites

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,50 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
-import MusicCard from '../components/MusicCard';
-import Carregando from '../components/Carregando';
+// import MusicCard from '../components/MusicCard';
+// import Carregando from '../components/Carregando';
 
 class Favorites extends Component {
-  constructor() {
-    super();
-    this.state = {
-      carregando: false,
-      listaFavoritas: [],
-      checked: true,
-    };
-  }
-
-  componentDidMount() {
-    this.musicasFavoritas();
-  }
-
-  musicasFavoritas = async () => {
-    this.setState({ carregando: true });
-    const chamaFunc = await getFavoriteSongs();
-    this.setState({ listaFavoritas: chamaFunc, carregando: false });
-  }
-
   render() {
-    const { listaFavoritas, carregando, checked } = this.state;
-
     return (
       <div data-testid="page-favorites">
         <Header />
-        { carregando ? (<Carregando />)
-          : (
-            <div>
-              { listaFavoritas.map((item) => (
-                <div key={ item.trackId }>
-                  <MusicCard
-                    trackName={ item.trackName }
-                    previewUrl={ item.previewUrl }
-                    trackId={ item.trackId }
-                    favoritas={ this.musicasFavoritas }
-                    path="favorites"
-                    checked={ checked }
-                  />
-                </div>
-              ))}
-            </div>)}
       </div>
     );
   }
